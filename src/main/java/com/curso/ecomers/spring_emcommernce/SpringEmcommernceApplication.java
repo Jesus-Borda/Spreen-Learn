551acc202f45ef3d75311c60753301ad23b527d5
Jesus Borda <jesusdeveloper2314@gmail.com>
2026-03-20 11:52:19 -0500

    Crecion de Model

--- src/main/java/com/curso/ecomers/spring_emcommernce/SpringEmcommernceApplication.java
+++ src/main/java/com/curso/ecomers/spring_emcommernce/SpringEmcommernceApplication.java
@@ -2,8 +2,9 @@ package com.curso.ecomers.spring_emcommernce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
 public class SpringEmcommernceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/curso/ecomers/spring_emcommernce/model/Usuario.java
@@ -0,0 +1,115 @@
+package com.curso.ecomers.spring_emcommernce.model;
+
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String apellido;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nombre, String username, String apellido, String email, String direccion, String telefono, String tipo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.apellido = apellido;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", username='" + username + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
