07db02808076ab6adb736663c94323ce45390dae
Jesus Borda <jesusdeveloper2314@gmail.com>
2026-03-20 10:54:19 -0500

    app empty

---
+++ src/main/java/com/curso/ecomers/spring_emcommernce/SpringEmcommernceApplication.java
@@ -0,0 +1,13 @@
+package com.curso.ecomers.spring_emcommernce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmcommernceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmcommernceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/ecomers/spring_emcommernce/SpringEmcommernceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.ecomers.spring_emcommernce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmcommernceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
