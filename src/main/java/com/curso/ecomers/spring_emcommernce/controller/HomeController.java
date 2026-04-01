d7b70f9d3397d884d018c02a889e42fa6a9f29ad
Jesus Borda <jesusdeveloper2314@gmail.com>
2026-04-01 11:32:58 -0500

    creacion de vista producto desde rol USUARIO

--- src/main/java/com/curso/ecomers/spring_emcommernce/controller/HomeController.java
+++ src/main/java/com/curso/ecomers/spring_emcommernce/controller/HomeController.java
@@ -2,17 +2,23 @@ package com.curso.ecomers.spring_emcommernce.controller;
 
 import com.curso.ecomers.spring_emcommernce.model.Producto;
 import com.curso.ecomers.spring_emcommernce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private  ProductoService productoService;
 
@@ -21,5 +27,16 @@ public class HomeController {
         model.addAttribute("productos",productoService.findAll() );
 
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome (@PathVariable Integer id, Model model){
+
+         log.info("ID producto enviado como parametro {} ", id);
+         Producto producto = new Producto();
+         Optional<Producto> productoOptional = productoService.get(id);
+         producto = productoOptional.get();
+         model.addAttribute("producto",producto);
+         return "usuario/productohome";
+
     }
 }
